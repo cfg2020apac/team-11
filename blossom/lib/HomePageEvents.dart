45083aa1385774c92b684602972858d054122345
Dalis Chan <dalischan97@gmail.com>
2020-10-10 02:30:10 +0800

    Style home page

--- blossom/lib/HomePageEvents.dart
+++ blossom/lib/HomePageEvents.dart
@@ -37,7 +37,6 @@ class _HomePageEventsState extends State<HomePageEvents> {
                 child: Card(
                     elevation: 5,
                     child : new InkWell(
-
                     onTap : (){
                       Navigator.push(
                         context,
@@ -48,6 +47,7 @@ class _HomePageEventsState extends State<HomePageEvents> {
                     child: Container(
                       decoration: BoxDecoration(
                           image: DecorationImage(
+                              colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.darken),
                               image: NetworkImage(list[index]['images'][0]),
                               fit: BoxFit.fitWidth,
                               alignment: Alignment.topCenter)),
