0a4193b1737908f6f56c9995aeba7326ae9eb477
Jeremy Wong <wong1134@e.ntu.edu.sg>
2020-10-10 02:46:07 +0800

    [feat] registration firebase auth working with sample UI

--- blossom/lib/main.dart
+++ blossom/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 import 'AboutPage.dart';
 import 'HomePage.dart';
